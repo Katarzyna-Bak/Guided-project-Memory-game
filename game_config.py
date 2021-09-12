bdad8d8c91958df37ee953d6f1f6d56dc644ea0e
Katarzyna-Bak <75198705+Katarzyna-Bak@users.noreply.github.com>
2021-09-12 09:24:44 +0200

    Add files via upload

---
+++ game_config.py
@@ -0,0 +1,12 @@
+import os
+
+IMAGE_SIZE = 128
+SCREEN_SIZE = 512
+NUM_TILES_SIDE = 4
+NUM_TILES_TOTAL = 16
+MARGIN = 4
+
+ASSET_FOLDER = 'C:/Users/katar/OneDrive/Desktop/Coding training/Python/Guided projects/Memory/assets'
+ASSET_FILES = [x for x in os.listdir(ASSET_FOLDER) if x[-3:].lower() == 'png']
+
+assert len(ASSET_FILES) == 8
